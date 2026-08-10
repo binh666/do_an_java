a477bd74198313f79c6914426bb0088f138c2395
binh666 <phambinh19323@gmail.com>
2026-08-10 12:00:25 +0700

    update

---
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -0,0 +1,65 @@
+package vn.hoidanit.laptopshop.domain;
+
+public class User {
+    private long id;
+    private String fullName;
+    private String email;
+    private String password;
+    private String phone;
+    private String address;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", fullName=" + fullName + ", email=" + email + ", password=" + password
+                + ", phone=" + phone + ", address=" + address + "]";
+    }
+
+}
